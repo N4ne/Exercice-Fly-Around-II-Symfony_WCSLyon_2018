32616f21654aada5aea768d435ca7fcda83ac24a
Aurelle Martin <martinaurelle.ma@gmail.com>
2018-05-03 15:11:17 +0200

    martin

--- src/AppBundle/Form/ReviewType.php
+++ src/AppBundle/Form/ReviewType.php
@@ -4,7 +4,14 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\EntityRepository;
+
 
 class ReviewType extends AbstractType
 {
@@ -13,7 +20,19 @@ class ReviewType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('text')->add('publicationDate')->add('note')->add('userRated')->add('reviewAuthor');
+        $builder
+            ->add('text', TextareaType::class, array('attr' => array('maxlength' => 250, 'label' => 'Description')))
+            ->add('publicationDate', DateType::class, array('data' => new \DateTime('now')))
+            ->add('note', IntegerType::class, array('attr' => array('min' => 0, 'max' => 5, 'label' => 'Note')))
+            ->add('agreeTerms', CheckboxType::class, array('mapped' => false))
+            ->add('userRated', EntityType::class, array(
+                'class' => 'AppBundle\Entity\User' ,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.lastName', 'ASC');
+                },
+                'choice_label' =>'lastName'))
+            ->add('reviewAuthor');
     }/**
      * {@inheritdoc}
      */
